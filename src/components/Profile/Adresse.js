66e77e9d065409da0d686e77e08c0964d1e6a6c5
Your Name <you@example.com>
2021-12-09 10:01:41 +0100

    first commit

---
+++ src/components/Profile/Adresse.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Adresse() {
+    return (
+        <div>
+            <h3> guerzanimayssa962@gmail.com</h3>
+        </div>
+    )
+}
+
+export default Adresse
+
+
+
+
---
+++ src/components/Profile/ProfilePhoto.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import myMayssa from './mayssa.jpg'
+
+function ProfilePhoto() {
+    return (
+        <div>
+            <img src={myMayssa} alt="logo"/>
+        </div>
+    )
+}
+
+export default ProfilePhoto
